412b22e078f1f1b0b416994608deb5c2a93920f7
habiboulaye <habiboulaye@gmail.com>
2013-08-05 13:25:39 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+
+##################################################################################
+# pyLxml version
+version='1.0'
+
+""" Traces """
+_Trace=False
+_Debug=True
+
+""" Error Codes """
+_ERROR_PROG=1
+_ERROR_CMD=2
+_ERROR_INPUT=3
