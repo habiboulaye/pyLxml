8555c6ee41b44a28e8c7282a17e1d8c135fa640a
habiboulaye <habiboulaye@gmail.com>
2013-08-05 13:30:24 +0200

    Update CreateSoft.py

--- CreateSoft.py
+++ CreateSoft.py
@@ -1,103 +1,138 @@
 #!/usr/bin/env python2
 # -*- coding: utf8 -*-
-"""
-    Library for XML processing
-    ->    class XML Tree Builders
+""" 
+ClassTemplet: model to create soft based-on pyLxml 
+
+Usage : 
+    - Replace ClassTemplet by your Class name 
+
+CMD: python CreateSoft.py in.xml out.xml
+
 """
 
-__author__ = "Habib"
-__version__ = "$Revision: 1.12 $"
-__date__ = "$Date: 2013/01/17 15:18:15 $"
-__copyright__ = "Copyright (c) Free 2013"
+__author__ = "Habib AMADOU BOUBACAR (hamadou@jouve.fr)"
+__version__ = "$Revision: 1.1 $"
+__date__ = "$Date: 2009/11/18 17:58:11 $"
+__copyright__ = "Copyright (c) free 2013"
 __license__ = "Python"
 
-import sys
-from lxml import etree as eTree
-from pyLxml import _Trace, _Debug, _ERROR_PROG
+#-------------Importation des modules -----------------------------------------------
+import time, sys
+
+#sys.path.append("Path/to/pyLxml/..")
+#import pyLxml
+#sys.path.remove("Path/to/pyLxml/..")
+
+from pyLxml.xmlParser import cXmlParser
+from pyLxml.xmlTree import cXmlTree
+from pyLxml import _Trace, _Debug, _ERROR_CMD, _ERROR_INPUT
+
+print "\t\t", time.strftime("%a, %d %b %Y %H:%M:%S +0000",time.localtime())
+
+#------------ Main class -----------------------------------------------------------
+class ClassTemplet(cXmlTree):
+    """
+         ClassTemplet: inheritance from cXmlTree
+    """
+    def __init__(self,inXML):
+        self._Trace, self._Debug=_Trace, _Debug
+        if bool(self._Debug):
+            print 'ClassTemplet: Init constructor'
+        cXmlTree.__init__(self, inXML)
+
+    def xmlProcessing(self, outFile, level):
+        """
+           Call cXmlParser:xmlWalker to parse the file 
+        """
+        if bool(self._Debug):
+            print 'ClassTemplet: xmlProcessing'
+        xmlParser = cXmlParser(self)
+        xmlParser.xmlwalker(self.root, level)
 
 
-##################################################################################
-class cXmlTree:
-    """
-        class XML Tree Builder
-    """
-    def __init__(self, inFileName):
-        if bool(_Trace):
-            print 'xmlDocTree: Init constructor'
-        try:
-            pT=eTree.XMLParser(remove_blank_text=False)
-            _docTree=eTree.parse(inFileName, parser=pT)
-        except Exception, Err:  #ERREUR !
-            sys.stderr.write("\nEERREUR LORS DU CHARGEMENT/PARSING DU FICHIER : EXISTENCE OU FORMAT-XML ??: MSG <%s>\n" %Err)
-            sys.exit(_ERROR_PROG) 
-            
-        self.docTree = _docTree
-        self.root = _docTree.getroot()
-        
-    """ Parser events """ 
-    def eventDoc(self, parser, elt):
-        if bool(_Debug):
-            print 'xmlDocTree: eventDoc'
-        pass
-    
+    # Events: rewrite event-functions that are needs
     def eventElement(self, parser, elt):
-        if bool(_Debug):
-            print ('xmlDocTree: eventElement')
-        pass
-               
+        """
+            Event -> Elements
+        """
+        if bool(self._Trace):
+            print 'ClassTemplet: eventElement[%s : %s]' %(elt.tag, elt.attrib)
+    # Process XML Elements
+    # Write your codes ...
+
     def eventPI(self, parser, elt):
-        if bool(_Debug):
-            print ('xmlDocTree: eventPI')
-        pass
+        """
+            Event -> Processing Instructions
+        """
+        if bool(self._Trace):
+            print 'ClassTemplet: eventPI[%s : %s]' %(elt.target, elt.text)
+    # Process XML Processing Instructions
+    # Write your codes ...
     
     def eventComment(self, parser, elt):
-        if bool(_Debug):
-            print ('xmlDocTree: eventComment')
-        pass
-   
-    def insertXmlElement(self, newChild, refChild, ipos=-1):
-        if (newChild==None) or (refChild==None):
-            sys.stderr.write("ERREUR-imput insertXmlElement: Element newChild or refChild is inconsistent \n\tMSGinfo: newChild=%s refChild=%s" %(newChild,refChild))
-            sys.exit(_ERROR_PROG)
-        try:
-            parent=refChild.getparent()
-            #print 'refChild.getparent()', refChild, refChild.getparent()
-            refIndex=parent.index(refChild)
-            if (ipos == -1):    # insertion de newChild Before<-1> refChild
-                parent.insert(refIndex,newChild)
-                return True
-            elif (ipos == +1):  # insertion de newChild After<+1> refChild
-                parent.insert(refIndex+1,newChild)
-                return True
-            elif (ipos == 0):   # Remplacement<0> de newChild par refChild
-                parent.insert(refIndex,newChild)
-                parent.remove(refChild)
-                return True
-        except Exception, Err:
-            sys.stderr.write("ERROR insertXmlElement: %s" %(Err))
-            sys.exit(_ERROR_PROG)
-    
-    def removeXmlElement(self, Child):
-        if (Child==None):
-            sys.stderr.write("WARRING-imput removeXmlElement: Element is incorrect \n\tMSGinfo: ELT=%s" %(Child))
-            #sys.exit(_ERROR_PROG)
-            return 0
-        try:
-            parent=Child.getparent()
-            parent.remove(Child)
-            return True
-        except Exception, Err:
-            sys.stderr.write("ERROR removeXmlElement: %s" %(Err))
-            sys.exit(_ERROR_PROG)
-
-    """ DTD validation """
-    def DTDvalid(self, dtdFile):
-        DTD = eTree.DTD(dtdFile)
-        if DTD.validate(self.root): return True
-        else:   return False
+        """
+            Event -> Comments
+        """
+        if bool(self._Trace):
+            print 'ClassTemplet: eventComment[%s : %s]' %(elt.tag, elt.text)
+        # Process XML Comments
+        # Write your codes ...
         
-    def printxmlDoc(self, outFile):
-        if bool(_Trace):
-            print 'xmlDocTree: printxmlDoc'
-        self.docTree.write(outFile,method="xml",xml_declaration=True,encoding="UTF-8",pretty_print=False)
 
+
+#--------------------------------------------------------------------------------------
+def usage():
+    """usage: print documentation"""
+    print( __doc__)
+    
+#--------------------------------------------------------------------------------------
+
+
+
+#------------ Main function-----------------------------------------------------------
+def main(argv):
+    print "\n\t *_*_*_*_* Start Execution *_*_*_*_*\n"
+    
+    LConsole = True
+    #---------------------------------------------------------------------------
+    if LConsole: # Linux Console
+        print ">>> Get commande line arguments"
+        if len(sys.argv) == 3:
+            inFile=sys.argv[1]
+            outFile = sys.argv[2]        
+        else:   #ERREUR !
+            sys.stderr.write("\nERROR COMMANDE :\n")
+            usage()
+            sys.exit(_ERROR_CMD)
+    else:   # Local
+        inFile = 'sample.in.xml'
+        outFile = 'sample.out.xml'
+       
+    #---------------------------------------------------------------------------
+    try:
+        infile=open(inFile, 'r')
+        infile.close()
+        print "inFile = [%s]" %(inFile.encode('UTF-8'))
+    except Exception, Err: #ERREUR !
+        sys.stderr.write("\nERROR: MSG <%s>\n" %Err)
+        sys.exit(_ERROR_INPUT)
+ 
+    #---------------------------------------------------------------------------
+    print ("\n>>> Instanciate ClassTemplet child of class cXmlTree") 
+    objTree = ClassTemplet(inFile)
+
+    #---------------------------------------------------------------------------
+    print ("\n>>> Process cXmlParser:xmlWalker on the instantied tree")
+    objTree.xmlProcessing(outFile, level=0)
+   
+    #---------------------------------------------------------------------------
+    print ("\n>>> Call cXmlTree:printxmlDoc to produce the (modified) XML output")
+    objTree.printxmlDoc(outFile)
+
+    #---------------------------------------------------------------------------
+    print ("\n\t *_*_*_*_* Successful Execution *_*_*_*_*\n\n")
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    print "\t\t", time.strftime("%a, %d %b %Y %H:%M:%S +0000",time.localtime())
+      
