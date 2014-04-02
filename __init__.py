f2f3a83814158671345eabc6ebb7eb0913afa75c
habiboulaye <habiboulaye@gmail.com>
2014-04-02 15:30:51 +0200

    Update __init__.py
    author contact update

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
     Library for XML processing
 """
 
-__author__ = "Habib"
+__author__ = "Habiboulaye AMADOU BOUBACAR (habiboulaye@gmail.com)"
 __version__ = "$Revision: 1.12 $"
 __date__ = "$Date: 2013/01/17 15:18:15 $"
 __copyright__ = "Copyright (c) Free 2013"
